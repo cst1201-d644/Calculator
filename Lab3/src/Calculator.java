e9c944edafb03f99dc984e9d6a286239824142a8
class <class@CST_N417_07.cst.citytech.cuny.edu>
2015-03-16 17:07:01 -0400

    Changed the code for the division

--- Lab3/src/Calculator.java
+++ Lab3/src/Calculator.java
@@ -17,40 +17,54 @@ public class Calculator {
      */
     public static void main(String[] args) {
         
-        double num1, num2;
+        int num1, num2;
         String operation;
         Scanner keyboard = new Scanner(System.in);
-
+        
         // Ask the user to enter an operator.
         Scanner operator = new Scanner(System.in);
-        System.out.println("Enter your operation:");
+        System.out.println("Enter operation to perform (+, -, *, /):");
         operation = operator.next();
         
         // Ask the user to enter an integer.
         System.out.println("Enter an integer:");
-        num1 = keyboard.nextDouble();
+        num1 = keyboard.nextInt();
 
         // Ask the user to enter another integer.
         System.out.println("Enter another integer:");
-        num2 = keyboard.nextDouble();
+        num2 = keyboard.nextInt();
 
+        // When addition is selected by the user.
         if (operation.equals("+"))
-        {
-    System.out.println("your answer is " + (num1 + num2));
-}
+            {
+                System.out.println("Your answer is " + (num1 + num2));
+            }
+        
+        // When subtraction is selected by the user.
         else if (operation.equals("-"))
-        {
-    System.out.println("your answer is " + (num1 - num2));
-}
-        else if (operation.equals("/"))
-        {
-    System.out.println("your answer is " + (num1 / num2));
-}
+                {
+                    System.out.println("Your answer is " + (num1 - num2));
+                    }
+        
+        // When multiplication is selected by the user.
         else if (operation.equals("*"))
-        {
-    System.out.println("your answer is " + (num1 * num2));
-}
-
-        }
-
-}
+                {
+                    System.out.println("Your answer is " + (num1 * num2));
+                }
+        
+        // When division is selected by the user.
+        // When the second integer is 0, operation will be terminated.
+        else if (operation.equals("/"))
+                {
+                    if (num2 == 0)
+                        System.out.println("Divided by 0. ERROR!!");
+                    else
+                        System.out.println("Your answer is " + (num1 / num2));
+                }
+        
+        // When none of the correct operators is selected.
+        else 
+        System.out.println(operation + " is not a valid operator!");
+            }
+        
+    }
