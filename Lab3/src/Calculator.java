824eb33f27b532d903d9580e80fe735f9cfadfff
23236859 <23236859@CST_N417_01.cst.citytech.cuny.edu>
2015-03-18 16:10:58 -0400

    Final version of the code

--- Lab3/src/Calculator.java
+++ Lab3/src/Calculator.java
@@ -23,33 +23,33 @@ public class Calculator {
         
         // Ask the user to enter an operator.
         Scanner operator = new Scanner(System.in);
-        System.out.println("Enter operation to perform (+, -, *, /):");
+        System.out.print("Enter an operator: ");
         operation = operator.next();
         
         // Ask the user to enter an integer.
-        System.out.println("Enter an integer:");
+        System.out.print("Enter an integer: ");
         num1 = keyboard.nextInt();
 
         // Ask the user to enter another integer.
-        System.out.println("Enter another integer:");
+        System.out.print("Enter another integer: ");
         num2 = keyboard.nextInt();
 
         // When addition is selected by the user.
         if (operation.equals("+"))
             {
-                System.out.println("Your answer is " + (num1 + num2));
+                System.out.println("The answer is: " + (num1 + num2));
             }
         
         // When subtraction is selected by the user.
         else if (operation.equals("-"))
                 {
-                    System.out.println("Your answer is " + (num1 - num2));
+                    System.out.println("The answer is: " + (num1 - num2));
                     }
         
         // When multiplication is selected by the user.
         else if (operation.equals("*"))
                 {
-                    System.out.println("Your answer is " + (num1 * num2));
+                    System.out.println("The answer is: " + (num1 * num2));
                 }
         
         // When division is selected by the user.
@@ -57,14 +57,28 @@ public class Calculator {
         else if (operation.equals("/"))
                 {
                     if (num2 == 0)
+                    {
                         System.out.println("ERROR: Cannot divide by 0.");
-                    else
-                        System.out.println("Your answer is " + (num1 / num2));
-                }
+                    }
+                    else if (num1 % num2 == 0)
+                    {
+                        System.out.println("The answer is: " + (num1 / num2));
+                    }
+                        else 
+                        {
+                            int division = num1 / num2;
+                            int fraction = num1 % num2;
+                            
+                            System.out.println("The answer is: " + division + " and "
+                                    + fraction + "/" + num2);
+                        }
+
+                }                
         
         // When none of the correct operators is selected.
-        else 
-        System.out.println("ERROR: " + operation + " is not a valid operator!");
-            }
-        
+        else                         
+            System.out.println("ERROR: " + operation + " is not a valid operator!");
+                        
     }
+    
+}
