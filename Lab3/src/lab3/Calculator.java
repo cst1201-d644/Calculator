f34a31075301770f359d63d28def3d6333365dda
Class <Class@CST_N919_24.cst.citytech.cuny.edu>
2015-03-18 11:32:29 -0400

    not finish yet

--- Lab3/src/lab3/Calculator.java
+++ Lab3/src/lab3/Calculator.java
@@ -6,7 +6,6 @@
 
 package lab3;
 
-import static java.time.Clock.system;
 import java.util.Scanner;
 
 /**
@@ -21,6 +20,31 @@ public class Calculator {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter an operator: ");
+        String operator = keyboard.next();
+        System.out.println("Enter an integer1: ");
+        int integer1 = keyboard.nextInt();
+        System.out.println("Enter an integer2: ");
+        int integer2 = keyboard.nextInt();
+        
+        
+        int math1 = integer1 + integer2;
+        int math2 = integer1 - integer2;
+        int math3 = integer1 * integer2;
+        int math4 = integer1 / integer2;
+        
+        if (operator.equals("+"))
+        System.out.println("Your answer is: " + math1); 
+        
+        else if (operator.equals("-"))
+        System.out.println("Your answer is: " + math2);
+        
+        if (operator.equals("*"))
+        System.out.println("Your answer is: " + math3);
+        
+        else if (operator.equals("/"))
+        System.out.println("Your answer is: " + math4);
+        
         
     }
     
