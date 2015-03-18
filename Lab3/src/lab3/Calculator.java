1f95a42417f72d095cdf71d9562c7c87b8de469f
class <class@CST_N417_06.cst.citytech.cuny.edu>
2015-03-18 17:47:59 -0400

    Lab 3

--- Lab3/src/lab3/Calculator.java
+++ Lab3/src/lab3/Calculator.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package lab3;
+
+import java.util.Scanner;
 
 /**
  *
@@ -16,7 +17,30 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter an operator:");
+        String operator = keyboard.next();
+        System.out.println("Enter an integer: ");
+        int integer1 = keyboard.nextInt();
+        System.out.println("Enter an integer:");
+        int integer2 = keyboard.nextInt();
+
+        int math1 = integer1 + integer2;
+        int math2 = integer1 - integer2;
+        int math3 = integer1 * integer2;
+        int math4 = integer1 / integer2;
+
+        if (operator.equals("+")) {
+            System.out.println("The answer is: " + math1);
+        } else if (operator.equals("-")) {
+            System.out.println("The answer is: " + math2);
+
+            if (operator.equals("*")) {
+                System.out.println("The answer is: " + math3);
+            } else if (operator.equals("/")) {
+                System.out.println("The answer is: " + math4);
+            }
+        }
     }
-    
+
 }
