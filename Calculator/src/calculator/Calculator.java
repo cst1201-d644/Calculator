a2a90ae06e3f79dc4260117b710a23a01644195a
prince1111 <anualli@10.1.192.202>
2015-03-18 22:10:14 -0400

    calculator

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -19,33 +19,49 @@ public class Calculator {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("print any one of these operators -,+,/,*");
+        // Ask user to print any one of these operation 
+        System.out.println("print any one of these operators with the keyboard -,+,/,*");
+        // user input stored 
         String userinput = keyboard.next();
+       // Ask you user for first number 
         System.out.println("enter an number");
         int num1 = keyboard.nextInt();
+        // Ask you user for another number 
         System.out.println(" another number");
         int num2 = keyboard.nextInt();
-
+        // If result equals 0 or remainder equals 0 print empty 
         int result = 0;
         int remainder = 0;
         String error = "";
-
-        if (userinput.equals("+")) {
-            result = num1 + num2;
-        } else if (userinput.equals("-")) {
-            result = num1 - num2;
-
-        } else if (userinput.equals("*")) {
-            result = num1 * num2;
-        } else if (userinput.equals("/")) {
-            if (num2 != 0) {
-                result = num1 / num2;
-                remainder = num1 % num2;
-            } else {
-                error = "can't dvide by zero";
-            }
-        } else {
-            error = "invalide operator";
+        // If user enters + calculate num1 + num2 
+        // if user enters - calculate num1 - num2 
+        // if user enters* calculate num1 * num2
+        // if user enters / check to see if num2 = 0 ,if not then calculate  result  num1 / num2 
+        // if user enters / check to see if num2 = 0 ,if not then  calculate remainder num1 % num2 
+        // if  num2 = 0 display an error message 
+        // if  remainder doesn't equal zero  print fraction +remainder / + num 2 
+          
+        
+        switch (userinput) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    remainder = num1 % num2;
+                } else {
+                    error = "can't dvide by zero";
+                }   break;
+            default:
+                error = "invalide operator";
+                break;
         }
 
         if (error.equals("")) {
